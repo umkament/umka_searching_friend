3474144b94e0a8c3da1fc1a82f5ebf0f164b1d61
umkament <97944243+umkament@users.noreply.github.com>
2023-12-25 15:40:49 +0300

    feat: added framer-motion

--- src/components/ui/dropDownMenu/dropDownMenu.stories.tsx
+++ src/components/ui/dropDownMenu/dropDownMenu.stories.tsx
@@ -1,1 +1,15 @@
-export default {}
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { DropDownMenu } from './dropDownMenu'
+
+const meta = {
+  argTypes: {},
+  component: DropDownMenu,
+  tags: ['autodocs'],
+  title: 'Components/DropDownMenu',
+} satisfies Meta<typeof DropDownMenu>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const DropDownMenuStory: Story = {}
--- src/components/ui/dropDownMenu/dropDownMenu.tsx
+++ src/components/ui/dropDownMenu/dropDownMenu.tsx
@@ -1,5 +1,7 @@
 import React, { ReactNode } from 'react'
 
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+
 export type DropDownMenuProps = {
   aline?: 'center' | 'end' | 'start'
   children?: ReactNode
@@ -14,5 +16,38 @@ export const DropDownMenu: React.FC<DropDownMenuProps> = ({
   onChange,
   trigger,
 }) => {
-  return true
+  return (
+    <DropdownMenu.Root onOpenChange={onChange} open={isMenuOpen}>
+      <DropdownMenu.Trigger asChild>
+        <button aria-label={'custom menu'} className={s.buttonTrigger}>
+          {trigger}
+        </button>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label />
+          <DropdownMenu.Item />
+          <DropdownMenu.Group>
+            <DropdownMenu.Item />
+          </DropdownMenu.Group>
+          <DropdownMenu.CheckboxItem>
+            <DropdownMenu.ItemIndicator />
+          </DropdownMenu.CheckboxItem>
+          <DropdownMenu.RadioGroup>
+            <DropdownMenu.RadioItem>
+              <DropdownMenu.ItemIndicator />
+            </DropdownMenu.RadioItem>
+          </DropdownMenu.RadioGroup>
+          <DropdownMenu.Sub>
+            <DropdownMenu.Portal>
+              <DropdownMenu.SubContent />
+            </DropdownMenu.Portal>
+          </DropdownMenu.Sub>
+          <DropdownMenu.Separator />
+          <DropdownMenu.Arrow />
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  )
 }
