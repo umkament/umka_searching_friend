cc83ec1fb94fcd11c3ad73889f130d61fa5be68f
umkament <97944243+umkament@users.noreply.github.com>
2024-01-22 15:30:59 +0300

    feat: start adding Slider, installed @radix-ui/react-slider

---
+++ src/components/ui/slider/slider.tsx
@@ -0,0 +1,11 @@
+export type SliderProps = {
+  label?: string
+  max?: number
+  min?: number
+  onChange?: (value: number[]) => void
+  step?: number
+  value?: number[]
+}
+export const Slider = ({ label, max, min, onChange, step = 1, value }: SliderProps) => {
+  return <></>
+}
