8469d22e44ab0a265657e6f17560c33d4ab07e34
umkament <97944243+umkament@users.noreply.github.com>
2024-01-23 11:10:18 +0300

    feat: creating pagination we need a hook to manage the pagination logic

--- src/components/ui/pagination/pagination.tsx
+++ src/components/ui/pagination/pagination.tsx
@@ -1,3 +1,34 @@
-export const Pagination = () => {
+type PaginationConditionals =
+  | {
+      onPerPageChang: (itemPerPage: number) => void
+      perPage: number
+      perPageOptions: number[]
+    }
+  | {
+      onPerPageChang?: never
+      perPage?: null
+      perPageOptions?: never
+    }
+
+export type PaginationProps = {
+  className?: string
+  count: number
+  onChange: (page: number) => void
+  onPerPageChang: (itemPerPage: number) => void
+  page: number
+  perPage?: null | number
+  perPageOptions?: number[]
+  siblings?: number
+} & PaginationConditionals
+export const Pagination = ({
+  className,
+  count = 10,
+  onChange,
+  onPerPageChang,
+  page = 1,
+  perPage = null,
+  perPageOptions,
+  siblings,
+}: PaginationProps) => {
   return <></>
 }
