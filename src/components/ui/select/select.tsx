36491fa9591cabe34e779dce289e1f3e2352ff9f
umkament <97944243+umkament@users.noreply.github.com>
2024-01-26 15:09:50 +0300

    ref: Select-component

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -1,5 +1,11 @@
 import { ReactNode } from 'react'
 
+import { Typography } from '@/components/ui/typography'
+import * as Label from '@radix-ui/react-label'
+import * as SelectRadixUI from '@radix-ui/react-select'
+
+import s from './select.module.scss'
+
 export type Options = {
   disabled?: boolean
   label?: string
@@ -15,6 +21,61 @@ export type SelectProps = {
   selectOptions: Options[]
   value?: string
 }
-export const Select = ({}: SelectProps) => {
-  return <></>
+export const Select = ({
+  className,
+  defaultValue,
+  disabled,
+  label,
+  onValueChange,
+  placeholder,
+  selectOptions,
+  value,
+}: SelectProps) => {
+  return (
+    <Label.Root>
+      <Typography
+        as={'label'}
+        className={`${s.label} ${disabled && s.labelDisabled}`}
+        variant={'body2'}
+      >
+        {label}
+      </Typography>
+      <SelectRadixUI.Root
+        defaultValue={defaultValue}
+        disabled={disabled}
+        onValueChange={onValueChange}
+        required
+        value={value}
+      >
+        <SelectRadixUI.Trigger>
+          <SelectRadixUI.Value />
+          <SelectRadixUI.Icon />
+        </SelectRadixUI.Trigger>
+
+        <SelectRadixUI.Portal>
+          <SelectRadixUI.Content>
+            <SelectRadixUI.ScrollUpButton />
+            <SelectRadixUI.Viewport>
+              <SelectRadixUI.Item>
+                <SelectRadixUI.ItemText />
+                <SelectRadixUI.ItemIndicator />
+              </SelectRadixUI.Item>
+
+              <SelectRadixUI.Group>
+                <SelectRadixUI.Label />
+                <SelectRadixUI.Item>
+                  <SelectRadixUI.ItemText />
+                  <SelectRadixUI.ItemIndicator />
+                </SelectRadixUI.Item>
+              </SelectRadixUI.Group>
+
+              <SelectRadixUI.Separator />
+            </SelectRadixUI.Viewport>
+            <SelectRadixUI.ScrollDownButton />
+            <SelectRadixUI.Arrow />
+          </SelectRadixUI.Content>
+        </SelectRadixUI.Portal>
+      </SelectRadixUI.Root>
+    </Label.Root>
+  )
 }
