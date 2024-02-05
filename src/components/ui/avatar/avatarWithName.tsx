6b4d66c8fb9d5ebcdf82f9d843a400da6af67d66
umkament <97944243+umkament@users.noreply.github.com>
2024-02-05 10:04:17 +0300

    add: component avatarWithName

--- src/components/ui/avatar/avatarWithName.tsx
+++ src/components/ui/avatar/avatarWithName.tsx
@@ -1,1 +1,16 @@
-export const AvatarWithName = () => {}
+import s from './avatar.module.scss'
+
+import { Avatar } from './avatar'
+
+type Props = {
+  avatar: string
+  name: string
+}
+export const AvatarWithName = ({ avatar, name }: Props) => {
+  return (
+    <div className={s.avatarWithNameWrapper}>
+      <span className={s.name}>{name}</span>
+      <Avatar avatar={avatar} />
+    </div>
+  )
+}
