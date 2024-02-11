559492bd3463a3af61dbde2d10e96e1a7e382a60
umkament <97944243+umkament@users.noreply.github.com>
2024-02-11 15:04:05 +0300

    ref: styles for avatar and header component

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,2 @@
 export { default as avatar } from './avatar.png'
-export { default as logoPNG } from './logo.png'
 export { default as logo } from './logo.svg'
--- src/components/ui/avatar/avatar.tsx
+++ src/components/ui/avatar/avatar.tsx
@@ -7,7 +7,7 @@ type Props = {
 }
 export const Avatar = ({ avatar, className, size = 36 }: Props) => {
   return (
-    <>
+    <div className={s.avatarContainer}>
       <img
         alt={'avatar'}
         className={`${s.avatar} ${className}`}
@@ -15,6 +15,6 @@ export const Avatar = ({ avatar, className, size = 36 }: Props) => {
         src={avatar}
         width={size}
       />
-    </>
+    </div>
   )
 }
