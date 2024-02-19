8870b893dce1f4d191425a32c458dd443dd867ff
umkament <97944243+umkament@users.noreply.github.com>
2024-02-19 13:29:35 +0300

    feat: create base-api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Provider } from 'react-redux'
+
+import { store } from '@/services/store'
+
 import { Router } from './router'
 
 export function App() {
-  return <Router />
+  return (
+    <Provider store={store}>
+      <Router />
+    </Provider>
+  )
 }
